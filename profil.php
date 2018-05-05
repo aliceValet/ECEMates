6e09b22f09e779f1c303b49c3ce78bb0fe083bcd
Clarraa <36473477+Clarraa@users.noreply.github.com>
2018-05-05 20:30:09 +0200

    Add files via upload

--- profil.php
+++ profil.php
@@ -1,95 +1,83 @@
-<meta charset="utf-8" />
 <?php
-$bdd = new PDO('mysql:host=127.0.0.1;dbname=ecemates;charset=utf8','root','');
+session_start();
+$bdd = new PDO('mysql:host=127.0.0.1;dbname=ecemates', 'root', '');
 
 
-if(isset($_FILES['avatar']) AND !empty($_FILES['avatar']['name']))
-{
 
-	$tailleMax = 2097152;
-	$extensionsValides = array('jpg','jpeg', 'gif', 'png');
-	if($_FILES['avatar']['size'] <= $tailleMax)
-	{
-		$extensionUpload = strtolower(substr(strrschr($_FILES['avatar']['name'], '.'), 1));
-		if(in_array($extensionUpload, $extensionsValides))
-		{
-				$chemin = "membres/avatars/".$_SESSION['id'].".".$extensionUpload;
-				$resultat = move_uploaded_file($_FILES['avatar']['tmp_name'], $chemin);
-				if($resulat)
-				{
-					$updateavatar = $bdd ->prepare('UPDATE membres SET avatar = :avatar WHERE id = :id');
-					$updateavatar ->execute (array(
-						'avatar' => $_SESSION['id'].".". $extensionUpload,
-						 'id' => $SESSION['id'] ));
-				}
-				else
-				{
-					$msg ="erreur importation";
-				}
-		}
-		else
-		 {
-		 	$msg = "mauvais format";
-		 }
-	}
-	else {
-		$msg = "votre photo ne doit pas dépasser 2Mo";
-	}
-}
+if(isset($_GET['id']) AND $_GET['id'] > 0) {
+   $getid = intval($_GET['id']);
+   $requser = $bdd->prepare('SELECT * FROM membres WHERE id = ?');
+   $requser->execute(array($getid));
+   $userinfo = $requser->fetch();
+
+
+
+
+if (isset ($_GET['id'])) { $variable = $_GET["id"]; }
+   else {
+      $variable = 1;
+   }
+
+$commentaire = "<a href='commentaire.php?id=".$variable." '>Accueil</a>";
+$profil= "<a href='profil.php?id=".$variable." '>Profil</a>";
+$photo= "<a href='photo.php?id=".$variable." '>Photos/videos</a>";
+
+$messagerie= "<a href='reception.php'>Messagerie</a>";
+$reseau = '<a href="reseau_pro.php">Reseau</a>';
+
+ 
+
+
 
 ?>
-<!DOCTYPE html>
+
+
+
+
 <html>
-<head>
-<title>Profil</title>
-
-<meta name="viewport" content="width=device-width,	
- initial-scale=1">
- <link rel="stylesheet"
- href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> 
-</head>
-
-<body>
-<a href="accueil.html" target="_blank"> <input type="button" value="Page d'acceuil"> </a>
-
-<a href="profil.html" target="_blank"> <input type="button" value="Profil"> </a>
-<a href="photo.html" target="_blank"> <input type="button" value="Photos/videos"> </a>
-<a href="messagerie.html" target="_blank"> <input type="button" value="Messagerie"> </a>
-<a href="reseau.html" target="_blank"> <input type="button" value="Réseau"> </a>
-<a href="notification.html" target="_blank"> <input type="button" value="Notifications"> </a>
-<a href="emplois.html" target="_blank"> <input type="button" value="Emplois"> </a>
-
-
-	
-		<table>
-			<tr>
-				<h3>Nom:</h3>
-			</tr>
-			<tr>
-				<td>Prénom:</td>	
-			</tr>
-			<tr>
-				<td>Pseudo:</td>	
-			</tr>
-			<tr>
-				<td>Année / Fonction:</td>
-	
-			</tr>		
-		</table>
-		<form method="POST" action ="" enctype="multipart/form-data">
-		<label> Avatar : </label>
-		 <input type="file" name="avatar"/>
-		 <input type="submit" value="mettre a jour le profil" name="mettre a jour" />
-		
-		</form>
-
-<div class="container">
- <h2> Compétences :</h2>
- <p> bla bla bla.</p>
+   <head>
+      <title>TUTO PHP</title>
+      <meta charset="utf-8">
+      <div id="menus">
+&bull;<?php echo $commentaire; ?> 
+&bull;<?php echo $profil; ?> 
+&bull;<?php echo $photo; ?> 
+&bull;<?php echo $messagerie; ?> 
+&bull;<?php echo $reseau; ?> 
+&bull;
 </div>
+   </head>
+   <body>
 
+      <div align="center">
+         <h2>Profil de <?php echo $userinfo['pseudo']; ?></h2>
+         <br /><br />
+         <?php
+         if (!empty($userinfo['avatar']))
+           {
+            ?>
+            <img src="membres/avatars/<?php echo $userinfo['avatar']; ?>" width="150" />
+          <?php
+           } 
+         ?>
+         <br/>
+         Pseudo = <?php echo $userinfo['pseudo']; ?>
+         <br />
+         Mail = <?php echo $userinfo['mail']; ?>
+         <br />
+         <?php
+         if(isset($_SESSION['id']) AND $userinfo['id'] == $_SESSION['id']) {
+         ?>
+         <br />
+         <a href="editionprofil.php">Editer mon profil</a>
+         <a href="deconnexion.php">Se déconnecter</a>
+         <?php
+         }
+         ?>
+      </div>
 
-</body>
+   </body>
 </html>
+<?php   
+}
+?>
