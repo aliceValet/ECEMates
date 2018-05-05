26b042fcc355c4413579e0a72e09348a9dde81e2
Clarraa <36473477+Clarraa@users.noreply.github.com>
2018-05-05 20:28:46 +0200

    Add files via upload

--- photo.php
+++ photo.php
@@ -1,35 +1,118 @@
 <?php
-
-$photo = isset($_POST["photo"])? $_POST["photo"] : ""; 
-$Image = "";
-
-$extensions = array ('.jpg');
-// //connecter l'utilisateur dans BDD
-// //votre serveur = localhost | votre login = root | votre mot de pass = ‘’ (rien)
-$pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
-$bdd = new PDO('mysql:host=localhost;dbname=ecemates', 'root', '', $pdo_options);
-
- if ($_POST['validation']){
-//création de la variable du dossier ou l'image sera enregistré
-$uploads_dir = 'C:\Users\cfran\Documents';
-//Récupération du nom de l'image
-        $name = $_FILES["imaged"]["name"];
-//mettre l'image dans le dossier precedemment donnée dans la variable et le nommé ensuite
-//par son nom recuperer aussi precedemment
-        move_uploaded_file($_FILES["imaged"]["tmp_name"], "$uploads_dir/$name");
-//mettre le chemin de l'image dans variable
-        $lien = "$uploads_dir/$name";
-        echo $lien;
-
-
-//l'inserer dans la BDD
-        $insertimg = $bdd->prepare("INSERT INTO photo(imaged) VALUES(?)");
-        $insertimg->execute(array($lien));
-       // echo  '<img src= "'.$data['image'].'" >';
- 
+session_start();
+$bdd = new PDO('mysql:host=127.0.0.1;dbname=ecemates', 'root', '');
+if(isset($_GET['id_photo']) AND $_GET['id_photo'] > 0) {
+   $getid = intval($_GET['id_photo']);
+   $requser = $bdd->prepare('SELECT * FROM photos_videos WHERE id_photo = ?');
+   $requser->execute(array($getid));
+   $userinfo = $requser->fetch();
+}
+if(isset($_FILES['image']) AND !empty($_FILES['image']['name']))
+{
+   $tailleMax = 2097152;
+   $extensionsValides = array('jpg','jpeg', 'gif', 'png');
+   if($_FILES['image']['size'] <= $tailleMax)
+   {
+      $extensionUpload = strtolower(substr(strrchr($_FILES['image']['name'], '.'), 1));
+      if(in_array($extensionUpload, $extensionsValides))
+      {
+            $chemin = "membres/photos/".$_SESSION['id_photo'].".".$extensionUpload;
+            $resultat = move_uploaded_file($_FILES['image']['tmp_name'], $chemin);
+            if($resultat)
+            {
+               $updateavatar = $bdd ->prepare('UPDATE photos_videos SET image = :image WHERE id_photo = :id_photo');
+               $updateavatar->execute (array(
+                  'image' => $_SESSION['id_photo'].".". $extensionUpload,
+                   'id_photo' => $_SESSION['id_photo'] 
+                ));
+               header('Location: photo.php?id='.$_SESSION['id_photo']);
+            }
+            else
+            {
+               $msg ="erreur importation";
+            }
+      }
+      else
+       {
+         $msg = "mauvais format";
+       }
+   }
+   else {
+      $msg = "votre photo ne doit pas dépasser 2Mo";
+   }
 }
 
+if (isset ($_GET['id'])) { $variable = $_GET["id"]; }
+   else {
+      $variable = 1;
+   }
 
+$commentaire = "<a href='commentaire.php?id=".$variable." '>Accueil</a>";
+$profil= "<a href='profil.php?id=".$variable." '>Profil</a>";
+$photo= "<a href='photo.php?id=".$variable." '>Photos/videos</a>";
 
+$messagerie= "<a href='reception.php'>Messagerie</a>";
 
 ?>
+
+
+<!DOCTYPE html>
+<html>
+   	<head>
+		<meta charset = “utf-8”>
+         
+      <div id="menus">
+    
+
+&bull;<?php echo $commentaire; ?> 
+&bull;<?php echo $profil; ?> 
+&bull;<?php echo $photo; ?> 
+&bull;<?php echo $messagerie; ?> 
+&bull;
+
+</div>
+<img src="logov.png" height="100" width="100"/>
+
+<title> Photos/vidéos </title>
+	</head>
+	<body>
+
+
+
+<form action="photo.php" method="post" enctype="multipart/form-data">
+        <p>
+        	<h4> Vos photos : <h4><br />
+<img src="image1.jpg" alt="Paris" height="100" width="100"/>
+<br /><br />	
+               
+             <p>
+            <label for="image">Parcourir dans mes images : </label><input type="file" name="image" id="image" /><br />
+            <label for="validation">Ajouter cette nouvelle photo : </label><input type="submit" name="validation" id="validation" value="Ajouter" />
+        </p>
+        </p>
+</form>
+<?php
+$dir = 'membres/photos/*.{jpg,jpeg,gif,png}';
+$files = glob($dir,GLOB_BRACE);
+  
+echo 'Listing des images du repertoire miniatures <br />';
+foreach($files as $image)
+{ 
+   $f= str_replace($dir,'',$image);
+   echo $f.'<br />';
+}
+?>
+
+
+<?php
+if (!empty($userinfo['image']))
+           {
+            ?>
+            <img src="membres/photos/<?php echo $userinfo['image']; ?>" width="150" />
+          <?php
+           } 
+         ?>
+
+
+	</body>
+</html>
